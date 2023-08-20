8b6f3dd180b2d42c92ed21de5e1552edcf90334a
Liuzhijian <Liuzhijian001>
2023-08-20 21:18:59 +0800

    finish assignment1

---
+++ assignment1/group.cpp
@@ -0,0 +1,21 @@
+#include "group.h"
+
+void Group::addObject(int index, Object3D *object) {
+    assert(index < objectNum);
+    objects[index] = object;
+}
+
+bool Group::intersect(const Ray &r, Hit &h, float tMin) {
+    Hit hTmp;
+    float tTmp = -1;
+    bool flagHasInter = false;
+    for (int i = 0; i < objectNum; i++) {
+        auto obj = objects[i];
+        if (obj->intersect(r, hTmp, tMin) &&(hTmp.getT() < tTmp || tTmp == -1)) {
+            flagHasInter = true;
+            h = hTmp;
+            tTmp = h.getT();
+        }
+    }
+    return flagHasInter;
+}
